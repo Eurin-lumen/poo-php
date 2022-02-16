f3c7c4c5baae9a9b78bd6ae1df9d738a4ff4c086
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-02-16 16:57:06 +0100

    LE CHAINAGE

--- .dist/index.php
+++ .dist/index.php
@@ -1,4 +1,13 @@
 <?php
- // Utilisation des fonctionalités d'un objet
+ 
+// D’abord, l’exemple sans chaînage :
 $date = new DateTime;
-echo $date->format('d/m/Y');
+$newDate = $date->modify('+1 day');
+ 
+echo $date->format('d/m/Y').PHP_EOL;
+echo $newDate->format('d/m/Y').PHP_EOL;
+ 
+// Maintenant avec le chaînage. Nous exploitons directement
+// l'objet qui nous est retourné sans le stocker dans une variable :
+$formatedDate = $date->modify('+1 day')->format('d/m/Y');
+echo $formatedDate.PHP_EOL;
