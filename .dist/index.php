0c8ebb9b54d6823aba0f8868d669148db6e297e8
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-02-16 16:49:39 +0100

    Notion de réference

---
+++ .dist/index.php
@@ -0,0 +1,12 @@
+<?php
+ 
+// déclaration de référence à l’objet
+function foo(DateTime $date) {
+    $date->modify('+1 day'); // permet d'ajouter 1 jour à la date
+}
+ 
+$date = new DateTime;
+foo ($date);
+var_dump($date);
+
+// $date est maintenant au lendemain
