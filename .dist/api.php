c6415bc4c0bbbbe7c5bbd269d4bed6e9faa6388d
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-02-25 01:23:22 +0100

    Masquer la complexité de vos objets

--- .dist/api.php
+++ .dist/api.php
@@ -1,22 +1,14 @@
 <?php
 
 declare(strict_types=1);
-
+// Masquer la complexité de vos objets
 class Pont
 {
-  // $unite ne sert que dans la classe, on met cette propriété en privé.
    private string $unite = 'm²';
   
-   public float $longueur;
-   public float $largeur;
-  
-   public function getSurface(): string
-   {
-       return ($this->longueur * $this->largeur) . $this->unite; // on renvoie l’unité en plus de la surface
-   }}
+   private float $longueur;
+   private float $largeur;
+}
 
 $towerBridge = new Pont;
 $towerBridge->longueur = 286.0;
-$towerBridge->largeur = 15.0;
-
-echo $towerBridge->getSurface();
