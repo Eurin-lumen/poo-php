c233b4e23de3a7396d61c287d7b6186e102df997
Eurin-lumen <eurindelagoldcoast@gmail.com>
2022-03-08 00:23:36 +0100

    le mot clé const pour les valeur immuable

---
+++ constantes.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+class Pont
+{
+   private const UNITE = ‘m²’;
+   private float $longueur;
+   private float $largeur;
+  
+   public function getSurface(): string
+   {
+       return ($this->longueur * $this->largeur) . self::UNITE;
+   }
+  // setLongueur et setLargeur ne changent pas
+}
+
+$towerBridge = new Pont;
+$towerBridge->setLongueur(286.0);
+$towerBridge->setLargeur(15.0);
+
+echo $towerBridge->getSurface();
